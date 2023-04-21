8329b2f665e253c83e2d27bd6ff71d6472093bfe
nicole1020 <annabellesmom1>
2023-04-21 12:30:58 -0700

    4.21 wills save model, and line 378 fix that

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ Introduces Gensim's Doc2Vec model and demonstrates its use on the
 `Lee Corpus <https://hekyll.services.adelaide.edu.au/dspace/bitstream/2440/28910/1/hdl_28910.pdf>`__.
 
 """
-
+import csv
 import logging
 
+from imblearn.over_sampling import SMOTE
+
+from sklearn.metrics import classification_report
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 ###############################################################################
@@ -132,41 +135,113 @@ import os
 import gensim
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+import seaborn as sns
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from nltk import word_tokenize, SnowballStemmer
+from nltk.corpus import stopwords
+from sklearn.naive_bayes import MultinomialNB
 
 # Set file names for train and test data
 test_data_dir = os.path.join(gensim.__path__[0], 'test', 'test_data')
 csv_train_file = os.path.join(test_data_dir, 'complaints_processed.csv')
-csv_test_file = os.path.join(test_data_dir, 'lee.cor')
+csv_test_file = os.path.join(test_data_dir, 'emails from Seattle Jewelry Company.csv')
+csv_tmp_file = os.path.join(test_data_dir, 'data_part_.csv')
+
+# issue with values
+# https://www.youtube.com/watch?v=OS2m0f2gVJ0
+missing_narrative = ['N/a', "Nan", "NaN", np.nan, "na", "Na", None]
 
 # needed to ignore first column (importing duplicate first col)
 # https://www.statology.org/pandas-read-csv-ignore-first-column/
-with open(csv_train_file) as x:
-    cols = len(x.readline().split(','))
 
-df = pd.read_csv(csv_train_file, usecols=range(1, cols))
 
+df_iterator = pd.read_csv(
+    csv_train_file,
+    chunksize=10000)
+
+for i, df_chunk in enumerate(df_iterator):
+    # Set writing mode to append after first chunk
+    mode = 'w' if i == 0 else 'a'
+
+    # Add header if it is the first chunk
+    header = i == 0
+
+    df_chunk.to_csv(
+        csv_tmp_file,
+        index=False,  # Skip index column
+        header=header,
+        mode=mode)
+
+df = pd.read_csv(csv_tmp_file)
+pd.set_option('display.max_colwidth', None)
+plt.show()
 print(df)
 print(df['product'].value_counts())
+print(df.isnull().sum())
+print(df.isnull().any())
+# display NaN values and product number
+nan_values = df[df['narrative'].isna()]
+print(nan_values)
+# drop wasn't working, needed to add parameter
+# https://stackoverflow.com/questions/49712002/pandas-dropna-function-not-working
+df.dropna(inplace=True)
 
-X_train, X_test, y_train, y_test = train_test_split(df['product'], df['narrative'], test_size=0.25, random_state=1111)
+df['product'].value_counts().plot(kind='bar')
+plt.bar(x='product', height=3.0, width=3.0)
+plt.xticks(rotation=10)
+plt.title('Email Description Counts')
+plt.show()
 
-vectorize = TfidfVectorizer()
+# check if drop worked
+complaints_dataframe = df[['product', 'narrative']]
+search_terms = {'credit_reporting': 0, 'debt_collection': 1, 'mortgages_and_loans': 2, 'credit_card': 3,
+                'retail_banking': 4
+                }
 
-X_train_vec = vectorize.fit_transform(X_train)
-X_test_vec = vectorize.transform(X_test)
+print(complaints_dataframe['product'].value_counts())
 
-from sklearn.naive_bayes import MultinomialNB
+complaints_dataframe['search_terms'] = complaints_dataframe['product'].map(search_terms)
+stemmer = SnowballStemmer(language='english')
+
+stop_words = stopwords.words("english")
+
+
+def tokenizer(text):
+    token = [word for word in word_tokenize(text) if
+             (len(word) > 3 and len(word.strip('Xx/')) > 2)]
+    tokens = map(str.lower, token)
+    stem = [stemmer.stem(item) for item in tokens if (item not in stop_words)]
+    return stem
+
+
+vectorize = TfidfVectorizer(analyzer=tokenizer)
+x_for = vectorize.fit_transform(df['narrative'][:8219].values.astype('U'))
+
+pd.set_option('display.max_colwidth', None)
+x_sm, y_sm = SMOTE().fit_resample(x_for, df['product'][:8219])
+X_train, X_test, y_train, y_test = train_test_split(x_sm,y_sm, test_size=0.3, random_state=0)
+print('this is xtrain', X_train.shape)
+print('this is xtest', X_test.shape)
+print('this is y_train', y_train.shape)
+print('this is y_test', y_test.shape)
+
+# issue with fit here
+# https://www.geeksforgeeks.org/ml-handling-imbalanced-data-with-smote-and-near-miss-algorithm-in-python/
+#followed SMOTE guide because i ran into an error with size of data differences.
+
 mnb = MultinomialNB()
-mnb.fit(X_train_vec, y_train)
 
+mnb.fit(X_train, y_train)
 
-mnb.predict(X_test)
-y_pred = mnb.predict(X_test_vec)
-confusion_matrix(y_test, y_pred)
+X_test_predict = mnb.predict(X_test)
+X_pred = mnb.predict(x_for)
+
+from sklearn.metrics import classification_report
+
+print(classification_report(y_test, X_test_predict))
 ###############################################################################
 # Define a Function to Read and Preprocess Text
 # ---------------------------------------------
@@ -199,7 +274,7 @@ def read_corpus(fname, tokens_only=False):
                 yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
 
 
-train_corpus = list(read_corpus(csv_train_file))
+train_corpus = list(read_corpus(csv_tmp_file))
 test_corpus = list(read_corpus(csv_test_file, tokens_only=True))
 
 ###############################################################################
@@ -233,8 +308,9 @@ print(test_corpus[:2])
 # However, this is a very very small dataset (300 documents) with shortish
 # documents (a few hundred words). Adding training passes can sometimes help
 # with such small datasets.
-#
-model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
+# adding max_vocab_size=50000 to reduce memory issue and size=300
+# https://stackoverflow.com/questions/59050644/memoryerror-unable-to-allocate-array-with-shape-and-data-type-float32-while-usi
+model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40, max_vocab_size=50000)
 
 ###############################################################################
 # Build a vocabulary
